3e0de19b543538f827579a0b78bb629ef7246f00
zhanghan <zhanghanyxx@163.com>
2022-10-28 11:44:28 +0800

    test 2

--- lib/web/base.py
+++ lib/web/base.py
@@ -3,9 +3,12 @@ from seleniumbase import BaseCase
 
 
 class Base(BaseCase):
+    def __init__(self):
+        super().__init__(*args, **kwargs)
+
     def setUp(self):
-        super(BaseCase, self).setUp()
+        super().setUp()
 
     def tearDown(self):
         self.save_teardown_screenshot()
-        super(BaseCase, self).tearDown()
+        super().tearDown()
